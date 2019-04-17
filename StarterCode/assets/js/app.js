04fab331ca22270baf91348fd02ba3a0aabd2478
Andrew Haug <44251652+BossHaug7@users.noreply.github.com>
2019-04-16 20:37:44 -0700

    Delete app.js

--- StarterCode/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-d3.csv("data.csv", function(error, healthData) {
-
-    // Log an error if one exists
-    if (error) return console.warn(error);
-  
-    // Print the tvData
-    console.log(healthData);
-
-});
